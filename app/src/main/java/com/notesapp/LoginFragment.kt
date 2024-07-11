2d1b8b465387edad49e36b8088bebebc65778615
agonzalesbm <andres.gonzales.dev@gmail.com>
2024-07-11 17:32:21 -0400

    [@agonzalesbm] Add: a toast and verification of valid account

--- app/src/main/java/com/notesapp/LoginFragment.kt
+++ app/src/main/java/com/notesapp/LoginFragment.kt
@@ -5,17 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
 import com.notesapp.databinding.FragmentLoginBinding
 import com.notesapp.viewmodels.LoginViewModel
+import kotlinx.coroutines.launch
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
     lateinit var binding: FragmentLoginBinding
     lateinit var loginViewModel: LoginViewModel
+    private val failLogin = "Login failed. Please try again."
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View? {
         binding = FragmentLoginBinding.inflate(inflater, container, false)
         return binding.root
@@ -27,8 +31,16 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
         binding.loginViewModel = loginViewModel
         binding.lifecycleOwner = this
         binding.loginButton.setOnClickListener {
-            loginViewModel.login()
-            binding.root.findNavController().navigate(R.id.action_loginFragment_to_homeFragment2)
+            lifecycleScope.launch {
+                val result = loginViewModel.login()
+                if (result == null) {
+                    Toast.makeText(context, failLogin, Toast.LENGTH_SHORT).show()
+                    return@launch
+                } else {
+                    binding.root.findNavController()
+                        .navigate(R.id.action_loginFragment_to_homeFragment2)
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/notesapp/viewmodels/LoginViewModel.kt
+++ app/src/main/java/com/notesapp/viewmodels/LoginViewModel.kt
@@ -4,12 +4,10 @@ import android.util.Patterns
 import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.notesapp.models.User
 import com.notesapp.repository.LoginRepository
-import kotlinx.coroutines.launch
 
-class LoginViewModel (val loginRepository: LoginRepository): ViewModel() {
+class LoginViewModel(val loginRepository: LoginRepository) : ViewModel() {
     var isValid = MediatorLiveData<Boolean>()
     var username = MutableLiveData<String>()
     var password = MutableLiveData<String>()
@@ -29,11 +27,9 @@ class LoginViewModel (val loginRepository: LoginRepository): ViewModel() {
                 Patterns.EMAIL_ADDRESS.matcher(username.value).matches()
     }
 
-    fun login() {
-        viewModelScope.launch {
-            val userName = username.value.toString()
-            val password = password.value.toString()
-            loginRepository.login(User(userName, password))
-        }
+    suspend fun login(): String? {
+        val userName = username.value.toString()
+        val password = password.value.toString()
+        return loginRepository.login(User(userName, password))
     }
 }
